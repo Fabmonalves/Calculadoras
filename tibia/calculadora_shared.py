742cfdd0625240075fdca4bf602495f03d2bbe67
Fabricio Monte <fab.alves46@gmail.com>
2023-03-02 21:08:27 -0300

    update

--- tibia/calculadora_shared.py
+++ tibia/calculadora_shared.py
@@ -1,3 +1,7 @@
+
+
+from modulos.format_valores import lines_
+
 my_level = int(input("Seu level: "))
 shared_min = (my_level / 3) * 2
 shared_max = (my_level / 2) * 3
@@ -5,5 +9,5 @@ shared_max = (my_level / 2) * 3
 
 
 print(f"Level minimo para sharear: \n{shared_min:.0f}" )
-print()
+lines_()
 print(f"Level maximo para sharear: \n{shared_max:.0f}" )
